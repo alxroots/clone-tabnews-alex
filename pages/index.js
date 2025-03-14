e923e11183e296b8e8e9f016806c36d93254b090
Alex Sousa <alexsousa@byteshouse.com>
2025-03-14 18:58:33 +0000

    update name

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
       <div>
         <h1>Desafio do meu curso.dev</h1>
-        <p>Olá MARCEL meu cunhadão (irmão né)! Tu sabes que tu mora no coração do leque-leque né? Só por isso que estas participando deste desafio.</p>
+        <p>Olá Marcel meu cunhadão (irmão né)! Tu sabes que tu mora no coração do leque-leque né? Só por isso que estas participando deste desafio.</p>
         <p>Mas para ganhar o premio tens que fazer uma selfie dizendo: Qual a combinação de quero-quero com pica-pau</p>
       </div>
   );
