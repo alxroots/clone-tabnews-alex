c203a21b6c739cac687187620caf62075f02ed6f
Alex Sousa <alexsousa@byteshouse.com>
2025-03-19 11:06:57 +0000

    add scripts  and '

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 function Home() {
   return (
-      <div>
-        <h1>Desafio do meu curso.dev</h1>
-        <p>Olá Marcel meu cunhadão (irmão né)! Tu sabes que tu mora no coração do leque-leque né? Só por isso que estas participando deste desafio.</p>
-        <p>Mas para ganhar o premio tens que fazer uma selfie dizendo: Qual a combinação de quero-quero com pica-pau</p>
-      </div>
+    <div>
+      <h1>Desafio do meu curso.dev</h1>
+      <p>
+        Olá Marcel meu cunhadão (irmão né)! Tu sabes que tu mora no coração do
+        leque-leque né? Só por isso que estas participando deste desafio.
+      </p>
+      <p>
+        Mas para ganhar o premio tens que fazer uma selfie dizendo: Qual a
+        combinação de quero-quero com pica-pau
+      </p>
+    </div>
   );
 }
 
