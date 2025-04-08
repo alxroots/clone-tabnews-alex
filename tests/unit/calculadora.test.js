290a7b75cd676e291121b09f141efce574cf27b6
Alex Sousa <alexsousa@byteshouse.com>
2025-04-08 17:41:36 +0100

    add integration test

--- tests/unit/calculadora.test.js
+++
@@ -1,6 +0,0 @@
-const calculadora = require("../../model/calculadora");
-
-test("Somar 2 + 2deveria  retornar 4", () => {
-  const resultado = calculadora.sum(2, 2);
-  expect(resultado).toBe(4);
-});
