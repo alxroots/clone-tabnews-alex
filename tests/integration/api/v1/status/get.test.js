01832beecd54edf29ceaab4cd10546319ced9450
Alex Sousa <alexsousa@byteshouse.com>
2025-04-14 19:40:28 +0100

    database in progress

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
+import database from "../../../../../infra/database";
+
 test("GET to /api/v1/status should return 200", async () => {
+  console.log("database", database);
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
